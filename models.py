db1551d90c7a28aa4b3f51058ec06a3515797124
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-07 17:45:55 +0400

    add admin and class File

---
+++ models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class File(models.Model):
+         name = models.CharField(max_length=100)
+         file = models.FileField(upload_to='files/')
+         uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+         uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
