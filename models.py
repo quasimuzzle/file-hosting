c4f68810d89a8dcc70998ec20f82304a132797a1
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-07 21:33:10 +0400

    add class comment and rating

--- models.py
+++ models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
-# модель данных для хранения файла
 class File(models.Model):
          name = models.CharField(max_length=100)
          file = models.FileField(upload_to='files/')
          uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
          uploaded_at = models.DateTimeField(auto_now_add=True)
 
+class Comment(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
+class Rating(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField()
