8e9eab764ea0fe8903975fd2fe06e0b323d29c83
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-07 21:40:30 +0400

    add file_list

--- views.py
+++ views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import File, Comment, Rating
+
+def file_list(request):
+    files = File.objects.all()
+    return render(request, 'files/file_list.html', {'files': files})
+
+# Другие представления для вывода комментариев, оценок и прочего
