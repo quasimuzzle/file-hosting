797274733092dc52601f609709755992f50ac780
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-07 23:53:11 +0400

    add 'description' in class File and FileForm

--- forms.py
+++ forms.py
@@ -4,10 +4,9 @@ from .models import File, Comment
 class FileForm(forms.ModelForm):
     class Meta:
         model = File
-        fields = ['name', 'description', 'file']
+        fields = ['name', 'description', 'file', 'description', 'uploaded_by'] # uploaded_by нада вообще?
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['text']
-        
--- models.py
+++ models.py
@@ -6,6 +6,7 @@ class File(models.Model):
          file = models.FileField(upload_to='files/')
          uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
          uploaded_at = models.DateTimeField(auto_now_add=True)
+         description = models.TextField() 
 
          class Meta:
               app_label = 'files' 
