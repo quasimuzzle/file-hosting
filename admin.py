e5d1f25d6d5bc9b545ce8b2dccfa8385a491bb24
quasimuzzle <anna.akhmyatzanova@gmail.com>
2024-01-07 21:36:30 +0400

    add register comment and rating

--- admin.py
+++ admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import File
+from .models import File, Comment, Rating
 
-     admin.site.register(File)
+admin.site.register(File)
+admin.site.register(Comment)
+admin.site.register(Rating)
+
